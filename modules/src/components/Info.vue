7e7140dd1cf1d176d3c9b647d0463b6b02152356
Kelven <kelvenbentofontes@gmail.com>
2022-11-16 21:51:12 -0300

    utilzando v-for em lista

--- modules/src/components/Info.vue
+++ modules/src/components/Info.vue
@@ -2,11 +2,13 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou Desempregado</p>
-        <p>Utilizo as seguintes Tecnologias: </p>
+        <p>Utilizo as seguintes Tecnologias para back-end: </p>
         <ul>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>Python</li>
+            <li v-for="(technology, index) in backend_technologies" v-bind:key="index">{{ technology }}</li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para front-end: </p>
+        <ul>
+            <li v-for="technology in frontend_technologies" v-bind:key="technology.id">{{ technology.language }}</li>
         </ul>
         <div>
             <button @click="showEmail">mostrar e-mail</button>
@@ -30,7 +32,14 @@ import Picture from './Picture.vue'
                 esta_trabalhando: true,
                 mostrar_email: false,
                 email: 'kelvenbentofontes@gmail.com',
-                meu_link: 'https://google.com'
+                meu_link: 'https://google.com',
+                backend_technologies: ['JavaScript', 'PHP', 'Python', 'Java'],
+                frontend_technologies: [
+                    {id:1, language: 'html'},
+                    {id:2, language: 'css'},
+                    {id:3, language: 'javaScript'},
+                    {id:4, language: 'Vue.js'}
+                ]
             }
         },
         methods: {
